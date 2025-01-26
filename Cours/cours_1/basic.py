fcd274f016eb2342c6ba3082dd0b1ea1b1fdb45c
CDAJasonteilliez <139145306+CDAJasonteilliez@users.noreply.github.com>
2025-01-26 14:46:42 +0100

    cours1

---
+++ Cours/cours_1/basic.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+
+root = tk.Tk()
+root.title("Tutorial Step 1 : Introduction")
+root.geometry("800x600")
+
+label  = tk.Label(root, text="Hello World!")
+label.pack()
+
+root.mainloop()
