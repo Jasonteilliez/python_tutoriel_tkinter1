127977628955b19e7b036d45fed1fef76450eaad
CDAJasonteilliez <139145306+CDAJasonteilliez@users.noreply.github.com>
2025-01-28 21:52:02 +0100

    Create frame.py

---
+++ Cours/cours_6/frame.py
@@ -0,0 +1,14 @@
+import tkinter as tk
+from PIL import ImageTk,Image
+
+root = tk.Tk()
+root.title("Tutorial Step 6 : Frame")
+# root.geometry("800x600")
+
+frame = tk.LabelFrame(root, padx=20, pady=20)
+frame.pack(padx=10, pady=10)
+
+b = tk.Button(frame, text="don't click here")
+b.pack()
+
+root.mainloop()
